ccfae50591e0e8a9b2d77dd264fc1a4d35bae5a1
Merola Giuseppe Pierpaolo <sennar.pierp@gmail.com>
2024-05-17 09:58:23 +0100

    Update main.R

--- scripts/main.R
+++ scripts/main.R
@@ -1,24 +1,6 @@
-print("---------------------------------------------------------CodeStart - SubGwas")
-
-args <- commandArgs(trailingOnly = TRUE)
-
-library("GenomicSEM")
-library("readr")
-library("dplyr")
-library("ggplot2")
-library("qqman")
-library("shru")
-library("data.table")
-library("utils")
-library("reshape2")
-library("devtools")
-library("corrplot")
-library("Matrix")
-
-
 files_input = c( #########################################################################
   "/scratch/prj/gwas_sumstats/cleaned/SCHI06.gz", 
-  "/users/k2473476/proj/NewGwasSub/DEPR14_ori_modified.gz", 
+  "/users/k2473476/proj/Recover/DEPR14_ori_modified.gz", 
   "/scratch/prj/gwas_sumstats/cleaned/BIPO03.gz")
 ref_file = "~/proj/CommonFiles/reference.1000G.maf.0.005.txt.gz" ####################################################
 hm3 = "~/proj/CommonFiles/w_hm3.snplist"  ############################################################
@@ -49,6 +31,23 @@ ncores = 32 ############################################################
 
 
 
+
+print("---------------------------------------------------------CodeStart - SubGwas")
+
+args <- commandArgs(trailingOnly = TRUE)
+
+library("GenomicSEM")
+library("readr")
+library("dplyr")
+library("ggplot2")
+library("qqman")
+library("shru")
+library("data.table")
+library("utils")
+library("reshape2")
+library("devtools")
+library("corrplot")
+library("Matrix")
 
 
 get_script_directory <- function() {
@@ -87,16 +86,16 @@ get_script_directory <- function() {
 script_directory <- get_script_directory()
 cat(script_directory, "\n")
 
-setwd(script_directory) ###############################################################################
+setwd(script_directory)
 
 new_dir_name <- args[1]
 writeLines(new_dir_name, "dir.txt")
-new_dir_path <- file.path(script_directory, new_dir_name) ##############################################################################
+new_dir_path <- file.path(script_directory, new_dir_name)
 if (!dir.exists(new_dir_path)) {
   dir.create(new_dir_path)
 }
 setwd(new_dir_path)
-
+print(new_dir_path)
 
 
 
@@ -116,7 +115,7 @@ setwd(new_dir_path)
 
  output_name_gz <-paste0(output_name, ".gz")
 
- corr_input <- paste0(paths_corr, corr$code) ############# path to munged sumstats
+ corr_input <- paste0(paths_corr, corr$code)
  corr_input <- c(corr_input)
  corr_input <- c(corr_input, output_name_gz)
 
@@ -192,6 +191,106 @@ print(model_LD)
 output_file <- "model_LD.csv"
 write.csv(model_LD, file = output_file, row.names = FALSE)
 cat("Output exported to", output_file)
+
+  psychosis_on_bd <- round(model_LD$results[1, "STD_Genotype"], 2)
+  mania_on_bd <- round(model_LD$results[3, "STD_Genotype"], 2)
+  depression_on_bd <- round(model_LD$results[4, "STD_Genotype"], 2)
+  psychosis_on_depression <- round(model_LD$results[6, "STD_Genotype"], 2)
+  
+  square_size <- 1       # Size of the squares
+  circle_radius <- 0.5   # Radius of the circles
+  x_distance <- 3        # Horizontal distance between the centers of the shapes
+  y_distance <- 1        # Vertical distance between the rows
+  
+  square_labels <- c(traitnames[2], traitnames[3], traitnames[1])
+  circle_labels <- c(latentnames[2], latentnames[3], latentnames[1])
+  arrow_labels <- c("1.00", mania_on_bd, "1.00", depression_on_bd, psychosis_on_bd, psychosis_on_depression)
+
+  squares <- data.frame(
+    x = rep(c(1, 1 + x_distance, 1 + 2 * x_distance), each = 4),
+    y = rep(0, 12),
+    group = rep(1:3, each = 4)
+  )
+  
+  squares <- squares %>%
+    group_by(group) %>%
+    mutate(
+      x = x + c(-square_size / 2, square_size / 2, square_size / 2, -square_size / 2),
+      y = y + c(-square_size / 2, -square_size / 2, square_size / 2, square_size / 2)
+    )
+  
+  circles <- data.frame(
+    x = rep(c(1, 1 + x_distance, 1 + 2 * x_distance), each = 100),
+    y = rep(square_size + y_distance, 300),
+    angle = rep(seq(0, 2 * pi, length.out = 100), 3),
+    group = rep(1:3, each = 100)
+  )
+  
+  center_square <- data.frame(
+    x = c(1 + x_distance, 1 + x_distance, 1 + x_distance, 1 + x_distance),
+    y = c(2 * square_size + 2 * y_distance, 2 * square_size + 2 * y_distance, 2 * square_size + 2 * y_distance, 2 * square_size + 2 * y_distance),
+    group = 1
+  )
+  
+  center_square <- center_square %>%
+    group_by(group) %>%
+    mutate(
+      x = x + c(-square_size / 2, square_size / 2, square_size / 2, -square_size / 2),
+      y = y + c(-square_size / 2, -square_size / 2, square_size / 2, square_size / 2)
+    )
+  
+  square_labels_df <- data.frame(
+    x = c(1, 1 + x_distance, 1 + 2 * x_distance),
+    y = rep(-square_size/2 - 0.2, 3),
+    label = square_labels
+  )
+  
+  circle_labels_df <- data.frame(
+    x = c(1, 1 + x_distance, 1 + 2 * x_distance),
+    y = rep(circle_radius + 2*y_distance + 0.2, 3),
+    label = circle_labels
+  )
+
+  ggplot() +
+    geom_polygon(data = squares, aes(x = x, y = y, group = group), fill = NA, color = "black") +
+    geom_polygon(data = center_square, aes(x = x, y = y, group = group), fill = NA, color = "black") +
+    geom_path(data = circles, aes(x = x + circle_radius * cos(angle), y = y + circle_radius * sin(angle), group = group), color = "black") +
+    geom_segment(aes(x = 1 + x_distance, y = 2 * y_distance + 1.5 * square_size, xend = 1, yend = circle_radius + 2*y_distance), arrow = arrow(length = unit(0.2, "cm")), color = "black") +
+    geom_segment(aes(x = 1 + x_distance, y = 2 * y_distance + 1.5 * square_size, xend = 1 + x_distance, yend = circle_radius + 2*y_distance), arrow = arrow(length = unit(0.2, "cm")), color = "black") +
+    geom_segment(aes(x = 1 + x_distance, y = 2 * y_distance + 1.5 * square_size, xend = 1 + 2 * x_distance, yend = circle_radius + 2*y_distance), arrow = arrow(length = unit(0.2, "cm")), color = "black") +
+    geom_segment(aes(x = 1 + 2 * x_distance, y = y_distance+circle_radius, xend = 4.5, yend = square_size/2), arrow = arrow(length = unit(0.2, "cm")), color = "black") +
+    geom_segment(aes(x = 1 + x_distance, y = y_distance+circle_radius, xend = 1 + x_distance, yend = square_size/2), arrow = arrow(length = unit(0.2, "cm")), color = "black") +
+    geom_segment(aes(x = 1, y = y_distance+circle_radius, xend = 4.5-square_size, yend = square_size/2), arrow = arrow(length = unit(0.2, "cm")), color = "black") +
+    geom_segment(aes(x = 1, y = y_distance+circle_radius, xend = 1, yend = square_size/2), arrow = arrow(length = unit(0.2, "cm")), color = "black") +
+    geom_segment(aes(x = 1 + 2 * x_distance, y = y_distance+circle_radius, xend = 1 + 2 * x_distance, yend = square_size/2), arrow = arrow(length = unit(0.2, "cm")), color = "black") +
+    geom_curve(aes(x = 1, y = circle_radius + 2*y_distance, xend = 1 + 2 * x_distance, yend = circle_radius + 2*y_distance), curvature = -0.15, arrow = arrow(length = unit(0.2, "cm")), color = "black") +
+    geom_curve(aes(x = 1-square_size/4, y = -0.5, xend = 1+square_size/4, yend = -0.5), curvature = 0.8, arrow = arrow(length = unit(0.2, "cm")), color = "black") +
+    geom_curve(aes(x = 4-square_size/4, y = -0.5, xend = 4+square_size/4, yend = -0.5), curvature = 0.8, arrow = arrow(length = unit(0.2, "cm")), color = "black") +
+    geom_curve(aes(x = 7-square_size/4, y = -0.5, xend = 7+square_size/4, yend = -0.5), curvature = 0.8, arrow = arrow(length = unit(0.2, "cm")), color = "black") +
+    geom_curve(aes(x = 0.57, y = 2-0.25, xend = 0.57, yend = 2+0.25), curvature = -0.8, arrow = arrow(length = unit(0.2, "cm")), color = "black") +
+    geom_curve(aes(x = x_distance+0.57, y = 2-0.25, xend = x_distance+0.57, yend = 2+0.25), curvature = -0.8, arrow = arrow(length = unit(0.2, "cm")), color = "black") +
+    geom_curve(aes(x = 2*x_distance+0.43+1, y = 2-0.25, xend = 2*x_distance+0.43+1, yend = 2+0.25), curvature = 0.8, arrow = arrow(length = unit(0.2, "cm")), color = "black") +
+    geom_curve(aes(x = 4-square_size/4, y = 4.5, xend = 4+square_size/4, yend = 4.5), curvature = -0.8, arrow = arrow(length = unit(0.2, "cm")), color = "black") +
+    geom_text(data = square_labels_df, aes(x = x, y = y+0.75, label = label), color = "black") +
+    geom_text(data = circle_labels_df, aes(x = x, y = y-0.75, label = label), color = "black") +
+    geom_text(aes(x = 4, y = 4, label = "Genetic Effect"), color = "black", size  = 3) +
+    geom_text(aes(x = 0.7, y = 0.7, label = arrow_labels[1]), color = "black", vjust = -0.5) +
+    geom_text(aes(x = 0.7 + x_distance, y = 1, label = arrow_labels[2]), color = "black", vjust = -0.5) +
+    geom_text(aes(x = 0.3 + 2*x_distance + square_size, y = 0.7, label = arrow_labels[3]), color = "black", vjust = -0.5) +
+    geom_text(aes(x = 2.4, y = 1, label = arrow_labels[4]), color = "black", vjust = -0.5) +
+    geom_text(aes(x = 2.6+x_distance, y = 1, label = arrow_labels[5]), color = "black", vjust = -0.5) +
+    geom_text(aes(x = 3.35, y = 2.9, label = arrow_labels[6]), color = "black", vjust = -0.5) +
+    geom_text(aes(x = 1, y = -1, label = "0.00"), color = "black", vjust = -0.5) +
+    geom_text(aes(x = 1+x_distance, y = -1, label = "0.00"), color = "black", vjust = -0.5) +
+    geom_text(aes(x = 1+2*x_distance, y = -1, label = "0.00"), color = "black", vjust = -0.5) +
+    geom_text(aes(x = 0.15, y = 1.9, label = "1.00"), color = "black", vjust = -0.5) +
+    geom_text(aes(x = 0.15+x_distance, y = 1.9, label = "1.00"), color = "black", vjust = -0.5) +
+    geom_text(aes(x = 1.85+2*x_distance, y = 1.9, label = "1.00"), color = "black", vjust = -0.5) +
+    geom_text(aes(x = 1+x_distance, y = 4.75, label = "2pq"), color = "black", vjust = -0.5) +
+    theme_void() +
+    coord_fixed()
+     
+  ggsave("SEMplot.png", width = 6, height = 6, units = "in")
 
 print("---------------------------------------------------------Part 2 Finished - Model")
 }
@@ -335,26 +434,30 @@ difftest.matrix <- function(mValues1, mStandard_errors1, mValues2, mStandard_err
   list(pTest.values = pTest.values, pTest.values.adj = pTest.values.adj)
 }
 
-# Read and prepare data
 mega_correlation <- readRDS("Correlation_output.rds")
+
+# Set up the trait names
 trait_names <- c(corr$trait, latentnames[3])
 original_order <- c(latentnames[3], corr$trait)
 
+# Prepare the S matrix
 S <- mega_correlation$S_Stand
 rownames(S) <- colnames(S) <- trait_names
 
+# Prepare the SE_R matrix
 SE_R <- matrix(nrow = nrow(S), ncol = ncol(S))
 rownames(SE_R) <- colnames(SE_R) <- trait_names
 SE_R[lower.tri(SE_R, diag = TRUE)] <- sqrt(diag(mega_correlation$V_Stand))
 SE_R[upper.tri(SE_R, diag = TRUE)] <- t(SE_R)[upper.tri(SE_R, diag = TRUE)]
 
+# Extract relevant correlations and standard errors
 R_mania <- S[latentnames[3], ]
 R_bd <- S[traitnames[3], ]
 seR_mania <- SE_R[latentnames[3], ]
 seR_bd <- SE_R[traitnames[3], ]
 
 # Perform differential test
-p_overall <- difftest.matrix(effectiveNumberOfTests = 35, mValues1 = as.matrix(R_mania), mStandard_errors1 = as.matrix(seR_mania), mValues2 = as.matrix(R_bd), mStandard_errors2 = as.matrix(seR_bd), mCorrelationEstimate.values = 0.897847609)
+p_overall <- difftest.matrix(effectiveNumberOfTests = length(trait_names), mValues1 = as.matrix(R_mania), mStandard_errors1 = as.matrix(seR_mania), mValues2 = as.matrix(R_bd), mStandard_errors2 = as.matrix(seR_bd))
 
 # Adjust p-values
 p_adjusted <- p_overall$pTest.values.adj[!is.na(p_overall$pTest.values.adj)]
@@ -383,17 +486,15 @@ colnames(merged_df)[5] <- paste0("SE_", traitnames[3])
 merged_df <- merged_df[match(original_order, merged_df$Trait), ]
 
 # Final modifications and save to CSV
-#merged_df <- merged_df[, -1]
-colnames(merged_df)[ncol(merged_df)] <- "Trait"
-merged_df <- merged_df[, c(ncol(merged_df), 1:(ncol(merged_df) - 1))]
-merged_df <- merged_df[-1, ]
-merged_df <- merged_df[rev(1:nrow(merged_df)), ]
-cluster <- c(corr$Cluster)
-merged_df$Cluster <- rev(cluster)
-
+# Remove Trait.1 column if it exists
 merged_df <- merged_df[, !grepl("Trait.1", colnames(merged_df))]
+
+# Cluster information
+cluster <- c(corr$Cluster)
+merged_df$Cluster <- cluster[match(merged_df$Trait, trait_names)]
+
 print(merged_df)
-write.csv(merged_df, "trait_correlation_data.csv", row.names = FALSE) 
+write.csv(merged_df, "trait_correlation_data.csv", row.names = FALSE)
 
 lines <- readLines("../dir.txt")
 
